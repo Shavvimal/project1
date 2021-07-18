97b91089925cb27025f69930a79b83c3dcb404fb
Shav <shavhugan@gmail.com>
2021-07-18 18:39:09 +0100

    added arrow to bottom of page and modal

---
+++ assets/script.js
@@ -0,0 +1,8 @@
+const modalClose = (modal) => {
+    const modalToClose = document.querySelector('.'+modal);
+    modalToClose.classList.remove('fadeIn');
+    modalToClose.classList.add('fadeOut');
+    setTimeout(() => {  
+        modalToClose.style.display = 'none';
+    }, 0);
+}

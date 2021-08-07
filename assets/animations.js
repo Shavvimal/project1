1188136549be80ec1b63c08034d74299d40c6b4f
Shav <shavhugan@gmail.com>
2021-08-07 18:30:08 +0100

    edited animations , changed ID's to classes for main bubbles and moved smaller bubbles higher to try and reducde clipping

--- assets/animations.js
+++ assets/animations.js
@@ -11,108 +11,67 @@ tl.from('#slide-1-content-left', {duration: 0.6, ease: "power2. out", opacity:0,
 tl.from('#illustration', {duration: 0.6, ease: "power2. out", opacity:0});
 
 let tlb1 = gsap.timeline({repeat: 200});
-tlb1.to('#bubble1', {
+tlb1.to('.bubble1', {
     duration: 3,
     y:4,
     ease: "power2. inout", 
 } );
-tlb1.to('#bubble1', {
+tlb1.to('.bubble1', {
     duration:3,
     y:0,
     ease: "power2. inout", 
 } )
 
-let tlbi1 = gsap.timeline({repeat: 200});
-tlbi1.to('#PortfolioLI', {
-    duration: 3,
-    y:15,
-    ease: "power2. inout", 
-} );
-tlbi1.to('#PortfolioLI', {
-    duration:3,
-    y:0,
-    ease: "power2. inout", 
-} )
+
 
 let tlb2 = gsap.timeline({repeat: 200});
-tlb2.to('#bubble2', {
+tlb2.to('.bubble2', {
     duration: 2.3,
     y:-5,
     ease: "power2. inout", 
 } );
-tlb2.to('#bubble2', {
+tlb2.to('.bubble2', {
     duration:2.3,
     y:0,
     ease: "power2. inout", 
 } )
 
-let tlb2i = gsap.timeline({repeat: 200});
-tlb2i.to('#AboutMeLI', {
-    duration: 2.3,
-    y:-16,
-    ease: "power2. inout", 
-} );
-tlb2i.to('#AboutMeLI', {
-    duration:2.3,
-    y:0,
-    ease: "power2. inout", 
-} )
 
 let tlb3 = gsap.timeline({repeat: 200});
-tlb3.to('#bubble3', {
+tlb3.to('.bubble3', {
     duration: 2.5,
     y:4,
     ease: "power2. inout", 
 } );
-tlb3.to('#bubble3', {
+tlb3.to('.bubble3', {
     duration:2.5,
     y:0,
     ease: "power2. inout", 
 } )
 
-let tlb3i = gsap.timeline({repeat: 200});
-tlb3i.to('#ContactLI', {
-    duration: 2.5,
-    y:15,
-    ease: "power2. inout", 
-} );
-tlb3i.to('#ContactLI', {
-    duration:2.5,
-    y:0,
-    ease: "power2. inout", 
-} )
+
 
 let tlb4 = gsap.timeline({repeat: 200});
-tlb4.to('#bubble4', {
+tlb4.to('.bubble4', {
     duration: 2,
     y:3,
     ease: "power2. inout", 
 } );
-tlb4.to('#bubble4', {
+tlb4.to('.bubble4', {
     duration:2,
     y:0,
     ease: "power2. inout", 
 } )
 
-let tlb4i = gsap.timeline({repeat: 200});
-tlb4i.to('#StackLI', {
-    duration: 2,
-    y:15,
-    ease: "power2. inout", 
-} );
-tlb4i.to('#StackLI', {
-    duration:2,
-    y:0,
-    ease: "power2. inout", 
-} )
 
 // Non Central Bubbles
 
 let tlb5 = gsap.timeline({repeat: 200});
 tlb5.to('#bubble5', {
     duration:0,
-    y:250,
+    y:270,
     ease: "none", 
+    
 } )
 tlb5.to('#bubble5', {
     duration: 11,
@@ -123,26 +82,28 @@ tlb5.to('#bubble5', {
 let tlb6 = gsap.timeline({repeat: 200});
 tlb6.to('#bubble6', {
     duration:0,
-    y:250,
+    y:270,
     ease: "none", 
 } )
 tlb6.to('#bubble6', {
     duration: 12,
-    y:-100,
+    y:-120,
     ease: "none", 
 } );
 
 let tlb7 = gsap.timeline({repeat: 200, delay: 1});
 tlb7.to('#bubble7', {
     duration:0,
-    y:110,
+    y:190,
     ease: "none", 
+    opacity: 0,
 } )
 tlb7.to('#bubble7', {
     duration: 14,
-    y:-220,
+    y:-230,
     ease: "none", 
 } );
+
 
 let tlb8 = gsap.timeline({repeat: 200, delay: 3});
 tlb8.to('#bubble8', {
@@ -216,7 +177,8 @@ tlb14.to('#bubble14', {
     duration: 17,
     y:-180,
     ease: "none", 
-} );
+} )
+
 
 let tlb15 = gsap.timeline({repeat: 200, delay: 0});
 tlb15.to('#bubble15', {
